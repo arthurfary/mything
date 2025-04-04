78f82d2410d086cb582005151387dc06ccd14092
arthurfary <arthurfary22@gmail.com>
2025-04-03 22:49:11 -0300

    api calls

--- src/lib/github-api-client.js
+++ src/lib/github-api-client.js
@@ -1,8 +1,6 @@
 import { Octokit } from "octokit";
 
-token = import.meta.env.GITHUB_TOKEN
-
-console.log(token)
+const token = import.meta.env.VITE_GITHUB_TOKEN
 
 const octokit_api = new Octokit({
   auth: token
